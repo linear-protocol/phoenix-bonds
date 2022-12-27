4a67564573215a404d1395780aaec6676de36406
dongcool <95206510+dongcool@users.noreply.github.com>
2022-12-27 11:03:58 +0800

    accrual info

--- contracts/phoenix-bonds/src/view.rs
+++ contracts/phoenix-bonds/src/view.rs
@@ -4,6 +4,16 @@ use crate::*;
 
 #[derive(Serialize)]
 #[serde(crate = "near_sdk::serde")]
+pub struct AccrualInfo {
+    alpha: Duration,
+    min_alpha: Duration,
+    adjust_interval: Duration,
+    adjust_rate: BasisPoint,
+    decreasing: bool,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "near_sdk::serde")]
 pub struct Summary {
     linear_balance: U128,
     reserve_pool_near_amount: U128,
@@ -12,7 +22,7 @@ pub struct Summary {
     treasury_pool_near_amount: U128,
     bootstrap_ends_at: Timestamp,
     tau: BasisPoint,
-    alpha: Duration,
+    accrual_parameter: AccrualInfo,
 }
 
 #[near_bindgen]
@@ -26,7 +36,14 @@ impl PhoenixBonds {
             treasury_pool_near_amount: self.treasury_pool_near_amount.into(),
             bootstrap_ends_at: self.bootstrap_ends_at,
             tau: self.tau,
-            alpha: self.accrual_param.current_alpha(current_timestamp_ms()),
+            accrual_parameter: AccrualInfo {
+                alpha: self.accrual_param.current_alpha(current_timestamp_ms()),
+                min_alpha: self.accrual_param.min_alpha,
+                adjust_interval: self.accrual_param.adjust_interval,
+                adjust_rate: self.accrual_param.adjust_rate,
+                decreasing: self.accrual_param.mean_length.mean(current_timestamp_ms())
+                    > self.accrual_param.target_mean_length as u128,
+            },
         }
     }
 }
