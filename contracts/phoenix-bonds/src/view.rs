80c7c388dedcd513783ed80e4106e02edc865cbc
dongcool <95206510+dongcool@users.noreply.github.com>
2023-01-03 11:42:41 +0800

    add more fields to summary

--- contracts/phoenix-bonds/src/view.rs
+++ contracts/phoenix-bonds/src/view.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 use near_sdk::{near_bindgen, serde::Serialize};
 
 use crate::*;
@@ -10,11 +12,14 @@ pub struct AccrualInfo {
     adjust_interval: Duration,
     adjust_rate: BasisPoint,
     decreasing: bool,
+    target_mean_length: Duration,
+    current_mean_length: Duration,
 }
 
 #[derive(Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Summary {
+    owner_id: AccountId,
     linear_balance: U128,
     reserve_pool_near_amount: U128,
     pending_pool_near_amount: U128,
@@ -28,7 +33,9 @@ pub struct Summary {
 #[near_bindgen]
 impl PhoenixBonds {
     pub fn get_summary(&self, linear_price: U128) -> Summary {
+        let current_ms = current_timestamp_ms();
         Summary {
+            owner_id: self.owner_id.clone(),
             linear_balance: self.linear_balance.into(),
             reserve_pool_near_amount: self.reserve_pool_near_amount(linear_price.0).into(),
             pending_pool_near_amount: self.pending_pool_near_amount.into(),
@@ -37,12 +44,19 @@ impl PhoenixBonds {
             bootstrap_ends_at: self.bootstrap_ends_at,
             tau: self.tau,
             accrual_parameter: AccrualInfo {
-                alpha: self.accrual_param.current_alpha(current_timestamp_ms()),
+                alpha: self.accrual_param.current_alpha(current_ms),
                 min_alpha: self.accrual_param.min_alpha,
                 adjust_interval: self.accrual_param.adjust_interval,
                 adjust_rate: self.accrual_param.adjust_rate,
-                decreasing: self.accrual_param.mean_length.mean(current_timestamp_ms())
+                decreasing: self.accrual_param.mean_length.mean(current_ms)
                     > self.accrual_param.target_mean_length,
+                target_mean_length: self.accrual_param.target_mean_length,
+                current_mean_length: self
+                    .accrual_param
+                    .mean_length
+                    .mean(current_ms)
+                    .try_into()
+                    .unwrap(),
             },
         }
     }
