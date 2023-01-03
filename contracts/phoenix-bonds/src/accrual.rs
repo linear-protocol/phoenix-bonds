19d624c65c6e0c94ea003b173acb106d8ebdc30d
dongcool <95206510+dongcool@users.noreply.github.com>
2023-01-03 14:29:07 +0800

    feat: change mean length type and add into summary (#40)
    * fix: change mean length type to duration
    
    * feat: add more fields to summary (#39)

--- contracts/phoenix-bonds/src/accrual.rs
+++ contracts/phoenix-bonds/src/accrual.rs
@@ -45,11 +45,11 @@ impl AccrualConfig {
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct AccrualParameter {
     /// last updated alpha value
-    pub alpha: u64,
+    pub alpha: Duration,
     /// minimum value that alpha could decrease to
-    pub min_alpha: u64,
+    pub min_alpha: Duration,
     /// target weighted mean length of all pending bonds
-    pub target_mean_length: u64,
+    pub target_mean_length: Duration,
     /// alpha decreases each interval after mean length exceeds target
     pub adjust_interval: Duration,
     /// how much should alpha decrease in each interval
@@ -62,9 +62,9 @@ pub struct AccrualParameter {
 
 impl AccrualParameter {
     pub fn new(
-        init_alpha: u64,
-        min_alpha: u64,
-        target_mean_length: u64,
+        init_alpha: Duration,
+        min_alpha: Duration,
+        target_mean_length: Duration,
         adjust_interval: Duration,
         adjust_rate: BasisPoint,
     ) -> AccrualParameter {
@@ -79,8 +79,8 @@ impl AccrualParameter {
         }
     }
 
-    pub fn current_alpha(&self, ts: Timestamp) -> u64 {
-        let current_mean_length: u64 = self.mean_length.mean(ts).try_into().unwrap();
+    pub fn current_alpha(&self, ts: Timestamp) -> Duration {
+        let current_mean_length = self.mean_length.mean(ts);
         if current_mean_length < self.target_mean_length {
             self.alpha
         } else {
@@ -108,9 +108,7 @@ impl AccrualParameter {
         self.mean_length.insert(amount, ts); // this could only make mean length shorter
         let new_mean_length = self.mean_length.mean(ts);
 
-        if old_mean_length >= self.target_mean_length as u128
-            && new_mean_length < self.target_mean_length as u128
-        {
+        if old_mean_length >= self.target_mean_length && new_mean_length < self.target_mean_length {
             self.alpha = alpha_before_insertion;
             self.last_updated_at = ts;
         }
@@ -123,10 +121,9 @@ impl AccrualParameter {
         self.mean_length.remove(amount, length, ts);
         let new_mean_length = self.mean_length.mean(ts);
 
-        if (old_mean_length >= self.target_mean_length as u128
-            && new_mean_length < self.target_mean_length as u128)
-            || (old_mean_length < self.target_mean_length as u128
-                && new_mean_length >= self.target_mean_length as u128)
+        if (old_mean_length >= self.target_mean_length && new_mean_length < self.target_mean_length)
+            || (old_mean_length < self.target_mean_length
+                && new_mean_length >= self.target_mean_length)
         {
             self.alpha = alpha_before_removal;
             self.last_updated_at = ts;
@@ -154,13 +151,16 @@ impl WeightedMeanLength {
     }
 
     /// Get volume weighted mean bonding length in ms at given timestamp
-    pub fn mean(&self, ts: Timestamp) -> u128 {
+    pub fn mean(&self, ts: Timestamp) -> Duration {
         require!(ts >= self.updated_at, ERR_BAD_TIMESTAMP);
         if self.total_weight == 0 {
             return 0;
         }
         let time_since_last_update = ts - self.updated_at;
-        (self.weighted_sum / self.total_weight.into() + time_since_last_update.into()).round_u128()
+        (self.weighted_sum / self.total_weight.into() + time_since_last_update.into())
+            .round_u128()
+            .try_into()
+            .unwrap()
     }
 
     fn update(&mut self, ts: Timestamp) {
@@ -209,50 +209,50 @@ mod tests {
         // - current va should be 1 day
         // - charles bonds 1000
         let ts = 2 * ONE_DAY_MS;
-        assert_eq!(va.mean(ts), ONE_DAY_MS as u128);
+        assert_eq!(va.mean(ts), ONE_DAY_MS);
 
         va.insert(1000 * ONE_NEAR, ts);
 
         // - va should be updated to 3/4 (0.75) day
-        assert_eq!(va.mean(ts), 3 * ONE_DAY_MS as u128 / 4);
+        assert_eq!(va.mean(ts), 3 * ONE_DAY_MS / 4);
 
         // ========== Day 6 ==========
         // - current va should be 19/4 (4.75) days
         // - alice bonds 2000
         let ts = 6 * ONE_DAY_MS;
-        assert_eq!(va.mean(ts), 19 * ONE_DAY_MS as u128 / 4);
+        assert_eq!(va.mean(ts), 19 * ONE_DAY_MS / 4);
 
         va.insert(2000 * ONE_NEAR, ts);
 
         // - va should be updated to 19/6 (3.167) days
-        assert_eq!(va.mean(ts), 19 * ONE_DAY_MS as u128 / 6);
+        assert_eq!(va.mean(ts), 19 * ONE_DAY_MS / 6);
 
         // ========== Day 11 ==========
         // - current va should be 49/6 (8.167) days
         // - bob cancels 2000, length is 10 days
         let ts = 11 * ONE_DAY_MS;
-        assert_eq!(va.mean(ts), 49 * ONE_DAY_MS as u128 / 6);
+        assert_eq!(va.mean(ts), 49 * ONE_DAY_MS / 6);
 
         va.remove(2000 * ONE_NEAR, 10 * ONE_DAY_MS, ts);
 
         // - va should be updated to 29/4 (7.25) days
-        assert_eq!(va.mean(ts), 29 * ONE_DAY_MS as u128 / 4);
+        assert_eq!(va.mean(ts), 29 * ONE_DAY_MS / 4);
 
         // ========== Day 16 ==========
         // - current va should be 49/4 (12.25) days
         // - alice commits 1000, length 15 days
         let ts = 16 * ONE_DAY_MS;
-        assert_eq!(va.mean(ts), 49 * ONE_DAY_MS as u128 / 4);
+        assert_eq!(va.mean(ts), 49 * ONE_DAY_MS / 4);
 
         va.remove(1000 * ONE_NEAR, 15 * ONE_DAY_MS, ts);
 
         // - va should be updated to 34/3 (11.33) days
-        assert_eq!(va.mean(ts), 34 * ONE_DAY_MS as u128 / 3);
+        assert_eq!(va.mean(ts), 34 * ONE_DAY_MS / 3);
 
         // ========== Day 21 ==========
         // - current va should be 49/3 (16.33) days
         let ts = 21 * ONE_DAY_MS;
-        assert_eq!(va.mean(ts), 49 * ONE_DAY_MS as u128 / 3);
+        assert_eq!(va.mean(ts), 49 * ONE_DAY_MS / 3);
     }
 
     fn prepare_accrual_param() -> AccrualParameter {
