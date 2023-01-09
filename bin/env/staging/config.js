1615f5ed9de1333ce36bd7030919b96364312309
dongcool <95206510+dongcool@users.noreply.github.com>
2023-01-09 20:02:36 +0800

    staging env

---
+++ bin/env/staging/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  network: {
+    networkId: "mainnet",
+    nodeUrl:
+      process.env.NEAR_CLI_MAINNET_RPC_SERVER_URL ||
+      "https://rpc.mainnet.near.org",
+    walletUrl: "https://wallet.mainnet.near.org",
+    helperUrl: "https://helper.mainnet.near.org",
+    explorerUrl: "https://explorer.mainnet.near.org",
+  },
+  contractId: "phoenix-staging.near",
+  ownerId: "phoenix-staging.near",
+  linearAddress: "linear-staging.near",
+  tau: 0.03,
+  bootstrapLength: 5 * 24, // hours
+  accrual: {
+    alpha: 1.4666, // days
+    minAlpha: 0.1, // days
+    targetMeanLength: 5, // days
+    adjustInterval: 24, // hours
+    adjustRate: 0.03,
+  },
+};
