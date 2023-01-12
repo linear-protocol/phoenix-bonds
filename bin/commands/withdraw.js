efce1771fdfa9ca7b12673d22d4a2f74d0144b64
dongcool <95206510+dongcool@users.noreply.github.com>
2023-01-12 14:59:48 +0800

    cli: treasury withdraw (#46)
    Co-authored-by: 0xdefacto <107129842+0xdefacto@users.noreply.github.com>

---
+++ bin/commands/withdraw.js
@@ -0,0 +1,41 @@
+const { Gas } = require("near-workspaces");
+const { getEnvConfig, storageDeposit } = require("../helper");
+const { init, funcCallProposal } = require("../near");
+
+exports.command = 'withdraw';
+exports.desc = 'Propose to withdraw treasury pool'
+exports.builder = yargs => {
+  yargs
+    .option('env', {
+      describe: 'Env name',
+      default: 'dev'
+    })
+    .option('signer', {
+      describe: 'Signer account ID'
+    })
+    .demandOption(['signer'])
+};
+
+exports.handler = async function (yargs) {
+  const { env, signer } = yargs;
+  const near = await init(env);
+
+  const config = getEnvConfig(env);
+  const contractId = config.contractId;
+  const signerAccount = await near.account(signer);
+
+  await storageDeposit(signerAccount, config.linearAddress, config.ownerId);
+
+  await funcCallProposal(
+    signerAccount,
+    config.ownerId,
+    'Withdraw treasury pool (160 Tgas)',
+    contractId,
+    'withdraw_treasury',
+    {},
+    '1',
+    Gas.parse('160 Tgas')
+  );
+
+  console.log('proposed');
+}
--- bin/helper.js
+++ bin/helper.js
@@ -10,8 +10,40 @@ function hoursToMs(n) {
   return parseInt(n * 3600 * 1000);
 }
 
+/**
+ * 
+ * @param {nearAPI.Near} near 
+ * @param {string} signer 
+ * @param {string} tokenId 
+ * @param {string} receiverId 
+ */
+async function storageDeposit (signer, tokenId, receiverId) {
+  const storageBalance = await signer.viewFunction(
+    tokenId,
+    'storage_balance_of',
+    {
+      account_id: receiverId
+    }
+  )
+  if (!storageBalance) {
+    await signer.functionCall({
+      contractId: tokenId,
+      methodName: 'storage_deposit',
+      args: {
+        account_id: receiverId
+      },
+      attachedDeposit: parseNearAmount('0.1')
+    });
+    console.log(`FT ${tokenId} storage deposited for ${receiverId}`);
+  } else {
+    console.log(`FT ${tokenId} is already registered for ${receiverId}`)
+  }
+}
+
+
 module.exports = {
   getEnvConfig,
   daysToMs,
   hoursToMs,
-};
+  storageDeposit,
+}
